18bd7479a2b8cd6ffd505cc4eb6a67eaaf441a41
jakerumbles <edwardsjake@live.com>
2022-08-17 12:31:43 -0500

    Some boilerplate setup

--- OCIF/ip-staking/src/lib.rs
+++ OCIF/ip-staking/src/lib.rs
@@ -21,9 +21,11 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/v3/runtime/frame>
+use sp_runtime::traits::{AtLeast32BitUnsigned};
+use scale_info::prelude::fmt::Display;
+
+use primitives::*;
+
 pub use pallet::*;
 
 // #[cfg(test)]
@@ -37,14 +39,35 @@ pub use pallet::*;
 
 #[frame_support::pallet]
 pub mod pallet {
-	use frame_support::{dispatch::DispatchResultWithPostInfo, pallet_prelude::*};
+	use super::*;
+	use frame_support::{dispatch::{DispatchResultWithPostInfo, DispatchErrorWithPostInfo}, pallet_prelude::*, traits::Currency, PalletId};
 	use frame_system::pallet_prelude::*;
+
+	pub type BalanceOf<T> = <T as pallet_balances::Config>::Balance;
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
-	pub trait Config: frame_system::Config {
+	pub trait Config: frame_system::Config + pallet_balances::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+
+		/// The IPS ID type
+        type IpsId: Parameter
+            + Member
+            + AtLeast32BitUnsigned
+            + Default
+            + Copy
+            + Display
+            + MaxEncodedLen
+            + Clone;
+
+		type Currency: Currency<Self::AccountId>;
+
+		#[pallet::constant]
+		type PalletId: Get<PalletId>; 
+
+		#[pallet::constant]
+		type IpsRegisterDeposit: Get<Self::Balance>;
 	}
 
 	#[pallet::pallet]
@@ -52,15 +75,11 @@ pub mod pallet {
 	pub struct Pallet<T>(_);
 
 	// The pallet's runtime storage items.
-	// https://docs.substrate.io/v3/runtime/storage
 	#[pallet::storage]
 	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
 	pub type Something<T> = StorageValue<_, u32>;
 
 	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/v3/runtime/events-and-errors
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
@@ -81,11 +100,51 @@ pub mod pallet {
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
+
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+
+		/// Register an IPS for IP Staking
+		#[pallet::weight(1)]
+		pub fn register(origin: OriginFor<T>, ips_id: T::IpsId) -> DispatchResultWithPostInfo {
+			let innovator = ensure_signed(origin)?;
+
+			Ok(().into())
+		}
+
+		/// Unregister an IPS for IP Staking
+		#[pallet::weight(1)]
+		pub fn unregister(origin: OriginFor<T>, ips_id: T::IpsId) -> DispatchResultWithPostInfo {
+			let innovator = ensure_signed(origin)?;
+
+			Ok(().into())
+		}
+
+		/// Stake towards an IPS
+		#[pallet::weight(1)]
+		pub fn stake(origin: OriginFor<T>, ips_id: T::IpsId, value: T::Balance) -> DispatchResultWithPostInfo {
+			let innovator = ensure_signed(origin)?;
+
+			Ok(().into())
+		}
+
+		/// Unstake from an IPS
+		#[pallet::weight(1)]
+		pub fn unstake(origin: OriginFor<T>, ips_id: T::IpsId, value: T::Balance) -> DispatchResultWithPostInfo {
+			let innovator = ensure_signed(origin)?;
+
+			Ok(().into())
+		}
+
+
+
+
+
+
+
+
+
+
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
 		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
